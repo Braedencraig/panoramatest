943d7f7373fa1e352c4c81efa2b3be91a86bc8aa
Braeden <bcraig@urbancoolab.com>
2021-03-15 19:44:01 -0400

    testing different format display

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { stack as Menu } from 'react-burger-menu'
 
 class Burger extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
-  }
-
   render () {
     return (
       <Menu right>
+        {/* <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul> */}
         <a id="home" className="menu-item" href="/">Home</a>
         <a id="about" className="menu-item" href="/about">About</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
       </Menu>
     );
   }
