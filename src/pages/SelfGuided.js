90d53414c41c587a073f0b00e36a785546f075a4
Braeden <bcraig@urbancoolab.com>
2021-04-18 21:06:57 -0400

    test

--- src/pages/SelfGuided.js
+++ src/pages/SelfGuided.js
@@ -51,15 +51,15 @@ export default function SelfGuided() {
   // panorama8 = new PANOLENS.ImagePanorama(one);
 
   // Link spheres
-  panorama1.link(panorama2, new THREE.Vector3(-207.5, 504.88, -6000.0), 600, `${imageTest}`);
+  panorama1.link(panorama2, new THREE.Vector3(-207.5, 504.88, -6000.0));
   // Edgecase for bug in panolens for second image useage, has to do with caching on their end.
-  panorama2.link(panorama3, new THREE.Vector3(-6907.5, 304.88, 1000.0), 600, `${imageTest}`);
+  panorama2.link(panorama3, new THREE.Vector3(-207.5, 504.88, -6000.0));
   // panorama2.link(panorama3, new THREE.Vector3(-6907.5, 304.88, 1000.0), 600, `${imageTest}`);
-  panorama3.link(panorama4, new THREE.Vector3(-207.5, 504.88, -6000.0), 600, `${imageTest}`);
-  panorama4.link(panorama5, new THREE.Vector3(-207.5, 504.88, -6000.0), 600, `${imageTest}`);
-  panorama5.link(panorama6, new THREE.Vector3(-207.5, 504.88, -6000.0), 600, `${imageTest}`);
-  panorama6.link(panorama7, new THREE.Vector3(-207.5, 504.88, -6000.0), 600, `${imageTest}`);
-  panorama7.link(panorama8, new THREE.Vector3(-207.5, 504.88, -6000.0), 600, `${imageTest}`);
+  panorama3.link(panorama4, new THREE.Vector3(-207.5, 504.88, -6000.0));
+  panorama4.link(panorama5, new THREE.Vector3(-207.5, 504.88, -6000.0));
+  panorama5.link(panorama6, new THREE.Vector3(-207.5, 504.88, -6000.0));
+  panorama6.link(panorama7, new THREE.Vector3(-207.5, 504.88, -6000.0));
+  panorama7.link(panorama8, new THREE.Vector3(-207.5, 504.88, -6000.0));
 
   // Create viewer and add panoramas
   viewer = new PANOLENS.Viewer({ autoHideInfospot: false });
